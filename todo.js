1e00650898cf135734dd354dd8fa296ccaf0e8df
Attila Bruder <bruderattila@gmail.com>
2021-11-23 21:12:01 +0100

    komment

--- todo.js
+++ todo.js
@@ -25,7 +25,7 @@ form.onsubmit = event => {
 }
 
 
-class Button {
+class Button {      //Ez lesz egy todo item gombja (azaz maga a todo elem) és ezen lesznek rajta a tulajdonságok (az objektum attribútumai)
     constructor(action, icon, type, title) {
         this.action = action; // a művelet, amit a gomb végez
         this.icon = icon; // a FontAwesome ikon neve (class="fas fa-*")
