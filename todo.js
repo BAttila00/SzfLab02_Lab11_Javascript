ce7343dff9f5859133970726afdab4b1f1e5b423
Attila Bruder <bruderattila@gmail.com>
2021-11-23 21:05:49 +0100

    memóriában hozzáadjuk a todo-t a listához. (Kiszedve a form-ból az értékét)

--- todo.js
+++ todo.js
@@ -11,3 +11,15 @@ const states = ["active", "inactive", "done"];
 const tabs = ["all"].concat(states);
 
 console.log(tabs);
+
+const form = document.getElementById("new-todo-form");
+const input = document.getElementById("new-todo-title");
+
+form.onsubmit = event => {
+    event.preventDefault(); // meggátoljuk az alapértelmezett működést, ami frissítené az oldalt
+    if (input?.value?.length) { // ha érvényes érték van benne -- ekvivalens ezzel: if (input && input.value && input.value.length) vagy if (input != null && input.value != null && input.value.length > 0)
+        todos.push(new Todo(input.value, "active")); // új to-do-t aktív állapotban hozunk létre
+        input.value = ""; // kiürítjük az inputot
+        // TODO: újrarajzolni a listát
+    }
+}
