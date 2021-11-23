f43689ae54eb9591efccfefdad753c1c782e18c7
Attila Bruder <bruderattila@gmail.com>
2021-11-23 21:16:12 +0100

    Todo item-ek megjelenítése. Kiszedjük a todos listából (memóriából ugyebár) és felrakjuk a html-re.

--- todo.js
+++ todo.js
@@ -42,3 +42,43 @@ const buttons = [ // a gombokat reprezentáló modell objektumok tömbje
     new Button("remove", "trash", "danger", "Remove"),
 ];
 
+
+function createElementFromHTML(html) {
+    const virtualElement = document.createElement("div");
+    virtualElement.innerHTML = html;
+    return virtualElement.childElementCount == 1 ? virtualElement.firstChild : virtualElement.children;
+}
+
+
+function renderTodos() {
+    const todoList = document.getElementById("todo-list"); // megkeressük a konténert, ahová az elemeket tesszük
+    todoList.innerHTML = ""; // a jelenleg a DOM-ban levő to-do elemeket töröljük
+    todos.forEach(todo => { // bejárjuk a jelenlegi todo elemeket (alternatív, funkcionális bejárással)
+        const row = createElementFromHTML(
+            `<div class="row">
+                <div class="col d-flex p-0">
+                    <a class="list-group-item flex-grow-1" href="#">
+                        ${todo.name}
+                    </a>
+                    <div class="btn-group action-buttons"></div>
+                </div>
+            </div>`);
+
+        buttons.forEach(button => { // a gomb modellek alapján legyártjuk a DOM gombokat
+            const btn = createElementFromHTML(
+                `<button class="btn btn-outline-${button.type} fas fa-${button.icon}" title="${button.title}"></button>`
+            );
+            if (todo.state === button.action) // azt a gombot letiljuk, amilyen állapotban van egy elem
+                btn.disabled = true;
+
+            // TODO: a gomb klikk eseményének kezelése
+
+            row.querySelector(".action-buttons").appendChild(btn); // a virtuális elem gomb konténerébe beletesszük a gombot
+        });
+
+        todoList.appendChild(row); // az összeállított HTML-t a DOM-ban levő #todo-list elemhez fűzzük
+    });
+}
+
+renderTodos(); // kezdeti állapot kirajzolása
+
